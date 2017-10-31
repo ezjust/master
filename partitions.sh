4765bcd73fec7e3fb5a39b598f616de91d71fe63
ezjust <3spirit3@ukr.net>
2017-10-31 15:46:15 +0200

    updated partitions.sh with 3rd disk

--- partitions.sh
+++ partitions.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+#Search for disks those would be used in whole script
+
+lsblk
+echo -e "!!!Please insert disks for partitions creation.\nSeparate them by coma, like /dev/sdb,/dev/sdc,/dev/sdd\nNOTE there should be three disks with minimum 2GB space on each!!!"
+read varname
+IFS=', ' read -r -a array <<< "$varname"
+echo "disk1=${array[0]}"\n"disk2=${array[1]}"\n"disk3=${array[2]}"\ndisks will be used for partitions creation
+
+disk1="${array[0]}"
+disk2="${array[1]}"
+disk3="${array[2]}"
+
 #Investigate if mdadm and lvm2 utilities exist
+
 if [ -n "`rpm -qa`" ]; then
   install=`rpm -qa | grep 'mdadm\|lvm' >> /dev/null 2>&1`
 else
@@ -9,7 +22,7 @@ fi
 
 if [ -n $install ]; then
   echo "------------------------------------------------"
-  echo "lvm and raid utilities exist and ready to use!!!"
+  echo "lvm2 and mdadm utilities exist and ready to use!!!"
   echo "------------------------------------------------"
 else 
   echo "---------------------------------------------------------------------"
@@ -18,148 +31,167 @@ else
   exit
 fi
 
-# Figlet utility installation
+#Figlet utility installation
 
-if [ -d /etc/zypp/ ];then
-  infig=`zypper install figlet`
-elif [ -d /etc/dpkg/ ]
-then
-  infig=`apt-get install figlet`
+pwd=$(pwd)
+wget ftp://ftp.figlet.org/pub/figlet/program/unix/ >> /dev/null 2>&1
+fig_link=$(cat index.html | grep "tar.gz" | sort -r | sed -n 1p | grep -oP '"\K.*?(?=")')
+wget $fig_link >> /dev/null 2>&1
+tar -xzvf figlet*.tar.gz >> /dev/null 2>&1
+cd figlet*
+make >> /dev/null 2>&1
+make install >> /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+find $pwd -name "figlet*.gz" -exec rm -rf {} \;
+find $pwd -name "index*.html*" -exec rm -rf {} \;
+figlet "figlet test"
+exit 0
 else
-  infig=`yum install figlet`
+echo "error occured on figlet compiling, check gcc compiler nad logs"
+exit 1
 fi
 
-if [ -n `dpkg -l figlet` ]; then
-  echo "----------------"
-  echo "figlet installed"
-  echo "----------------"
-elif [ -n `rpm -qa figlet` ]; then
-  echo "----------------"
-  echo "figlet installed"
-  echo "----------------"
-else
-  echo "----oops, some errors occurs-----"
-exit
-fi
 
 #Umount partitions and remove all mount points folders, wipe fs
 
-echo "------------------------------";
-echo "OPERATIONS with old partitions";
-echo "------------------------------";
+figlet "OPERATIONS with old partitions";
 
-umount /mnt/mp_ext2
-umount /mnt/mp_ext3
-umount /mnt/mp_ext4
-umount /mnt/mp_xfs
-umount /mnt/mp_btrfs
-umount /mnt/mp_md0
-umount /mnt/mp_lvm3
+umount -a >> /dev/null 2>&1
 
-rm -rf /mnt/*
+umount /mnt/mp_unaligned_xfs >> /dev/null 2>&1
+umount /mnt/mp_ext3 >> /dev/null 2>&1
+umount /mnt/mp_ext4 >> /dev/null 2>&1
+umount /mnt/mp_xfs >> /dev/null 2>&1
+umount /mnt/mp_btrfs >> /dev/null 2>&1
+umount /mnt/mp_md0 >> /dev/null 2>&1
+umount /mnt/mp_lvm3 >> /dev/null 2>&1
+umount /mnt/mp_unaligned_ext3 >> /dev/null 2>&1
+umount /mnt/mp_unaligned_ext4 >> /dev/null 2>&1
+umount /mnt/mp_unaligned_btrfs >> /dev/null 2>&1
 
-mdadm --stop /dev/md0
-mdadm --remove /dev/md0
+rm -rf /mnt/* >> /dev/null 2>&1
 
-wipefs -a /dev/sdb1
-wipefs -a /dev/sdb2
-wipefs -a /dev/sdb3
-wipefs -a /dev/sdb4
-wipefs -a /dev/sdc1
+mdadm --stop /dev/md0 >> /dev/null 2>&1
+mdadm --remove /dev/md0 >> /dev/null 2>&1
+
+wipefs -a ${disk1}1 >> /dev/null 2>&1
+wipefs -a ${disk1}2 >> /dev/null 2>&1
+wipefs -a ${disk1}3 >> /dev/null 2>&1
+wipefs -a ${disk1}4 >> /dev/null 2>&1
+wipefs -a ${disk2}1 >> /dev/null 2>&1
+wipefs -a ${disk3}1 >> /dev/null 2>&1
+wipefs -a ${disk3}2 >> /dev/null 2>&1
+wipefs -a ${disk3}3 >> /dev/null 2>&1
 
 wipefs -a /dev/lvm1/lvm1_xfs >> /dev/null 2>&1
-lvremove -f /dev/lvm1/lvm1_xfs
+lvremove -f /dev/lvm1/lvm1_xfs >> /dev/null 2>&1
 wipefs -a /dev/lvm2/lvm2_ext3 >> /dev/null 2>&1
-lvremove -f /dev/lvm2/lvm2_ext3
+lvremove -f /dev/lvm2/lvm2_ext3 >> /dev/null 2>&1
 wipefs -a /dev/lvm3/lvm3_ext4 >> /dev/null 2>&1
-lvremove -f /dev/lvm3/lvm3_ext4
+lvremove -f /dev/lvm3/lvm3_ext4 >> /dev/null 2>&1
 
-vgremove -f lvm1
-vgremove -f lvm2
-vgremove -f lvm3
+vgremove -f lvm1 >> /dev/null 2>&1
+vgremove -f lvm2 >> /dev/null 2>&1
+vgremove -f lvm3 >> /dev/null 2>&1
 
-pvremove -f /dev/sdc2
-pvremove -f /dev/sdc3
-pvremove -f /dev/sdc4
+pvremove -f ${disk2}2 >> /dev/null 2>&1
+pvremove -f ${disk2}3 >> /dev/null 2>&1
+pvremove -f ${disk2}4 >> /dev/null 2>&1
 
 
 for i in {1..4}
 do
 
-(echo d; echo $i; echo w;) | fdisk /dev/sdb >> /dev/null 2>&1
-sleep 0.1
-(echo d; echo $i; echo w;) | fdisk /dev/sdc >> /dev/null 2>&1
-sleep 0.1
+(echo d; echo $i; echo w;) | fdisk $disk1 >> /dev/null 2>&1
+sleep 0.2
+(echo d; echo $i; echo w;) | fdisk $disk2 >> /dev/null 2>&1
+sleep 0.2
+(echo d; echo $i; echo w;) | fdisk $disk3 >> /dev/null 2>&1
+sleep 0.2
+
 
 done
 
-sed -i '/mp\|lvm3_ext/d' /etc/fstab;
+sed -i '/mp\|lvm3_ext/d' /etc/fstab; >> /dev/null 2>&1
 sed -i '/md0/d' /etc/mdadm/mdadm.conf >> /dev/null 2>&1
 sed -i '/md0/d' /etc/mdadm.conf >> /dev/null 2>&1
 
-echo "-----------------------------";
-echo "Disks were successfully wiped";
-partprobe
-lsblk | grep sd[b-e]
-echo "-----------------------------";
-sleep 10
-#Make new partitions of two disks
-echo "------------------------------";
-echo "=======Creation section=======";
-echo "------------------------------";
-echo "!!!!!!!!!!!!!!2 disks should be sized at least 2GB!!!!!!!!!!!!!";
+figlet "Disks were successfully wiped"
+partprobe >> /dev/null 2>&1
+sleep 0.5
+lsblk 
+echo "-----------------------------"
 
-(echo n; echo p; echo 1; echo ; echo 1000000; echo n; echo p; echo 2; echo 1000001; echo 2000000; echo n; echo p; echo 3; echo 2000001; echo 3000000; echo n; echo p; echo 4; echo 3000001; echo 4000000; echo w;)  | fdisk /dev/sdb >> /dev/null 2>&1
+#Make new partitions on disks
 
-sleep 0.1
+figlet "===Creation section==="
 
-(echo n; echo p; echo 1; echo ; echo 1000000; echo n; echo p; echo 2; echo 1000001; echo 2000000; echo n; echo p; echo 3; echo 2000001; echo 3000000; echo n; echo p; echo 4; echo 3000001; echo 4000000; echo w;) | fdisk /dev/sdc >> /dev/null 2>&1
+(echo n; echo p; echo 1; echo 3; echo 1000000; echo n; echo p; echo 2; echo 1000001; echo 2000000; echo n; echo p; echo 3; echo 2000001; echo 3000000; echo n; echo p; echo 4; echo 3000001; echo 4000000; echo w;)  | fdisk $disk1 >> /dev/null 2>&1
 
+sleep 0.2
 
-sleep 0.1
+(echo n; echo p; echo 1; echo ; echo 1000000; echo n; echo p; echo 2; echo 1000001; echo 2000000; echo n; echo p; echo 3; echo 2000001; echo 3000000; echo n; echo p; echo 4; echo 3000001; echo 4000000; echo w;) | fdisk $disk2 >> /dev/null 2>&1
+
+sleep 0.2
+
+(echo n; echo p; echo 1; echo 2; echo 999999; echo n; echo p; echo 2; echo 1000000; echo 1999999; echo n; echo p; echo 3; echo 2000000; echo 2999999; echo n; echo p; echo 4; echo 3000000; echo 4000000; echo w;)  | fdisk $disk3 >> /dev/null 2>&1
+
+sleep 0.2
+
 
 #Create raid and lvm
 
-pvcreate /dev/sdc2
-vgcreate lvm1 /dev/sdc2
-lvcreate -n lvm1_xfs -l 100%FREE lvm1
-pvcreate /dev/sdc3
-vgcreate lvm2 /dev/sdc3
-lvcreate -n lvm2_ext3 -l 100%FREE lvm2
-pvcreate /dev/sdc4
-vgcreate lvm3 /dev/sdc4
-lvcreate -n lvm3_ext4 -l 100%FREE lvm3
+pvcreate ${disk2}2 >> /dev/null 2>&1
+vgcreate lvm1 ${disk2}2 >> /dev/null 2>&1
+lvcreate -n lvm1_xfs -l 100%FREE lvm1 >> /dev/null 2>&1
+pvcreate ${disk2}3 >> /dev/null 2>&1
+vgcreate lvm2 ${disk2}3 >> /dev/null 2>&1
+lvcreate -n ${disk2}3 -l 100%FREE lvm2 >> /dev/null 2>&1
+pvcreate ${disk2}4 >> /dev/null 2>&1
+vgcreate lvm3 ${disk2}4 >> /dev/null 2>&1
+lvcreate -n ${disk2}4 -l 100%FREE lvm3 >> /dev/null 2>&1
 
-(echo yes;) | mdadm --create --verbose /dev/md0 --level=1 --raid-disks=2 /dev/lvm1/lvm1_xfs /dev/lvm2/lvm2_ext3
+(echo yes;) | mdadm --create --verbose /dev/md0 --level=1 --raid-disks=2 /dev/lvm1/lvm1_xfs /dev/lvm2/lvm2_ext3 >> /dev/null 2>&1
  
 #Make fs on those partitions (ext2, ext3, ext4, xfs, btrfs)
 
-mkfs.ext2 /dev/sdb1
-mkfs.ext3 /dev/sdb2
-mkfs.ext4 /dev/sdb3
-mkfs.xfs /dev/sdb4
-mkfs.btrfs /dev/sdc1
+mkfs.xfs ${disk1}1
+mkfs.ext3 ${disk1}2
+mkfs.ext4 ${disk1}3
+mkfs.xfs ${disk1}4
+mkfs.btrfs ${disk2}1
 mkfs.xfs /dev/md0
 mkfs.ext4 /dev/lvm3/lvm3_ext4
+mkfs.ext3 ${disk3}1
+mkfs.ext4 ${disk3}2
+mkfs.btrfs ${disk3}3
+
 
 
 #Create folders for mount and mount available partitions
 
-mkdir /mnt/mp_ext2
-mkdir /mnt/mp_ext3
-mkdir /mnt/mp_ext4
-mkdir /mnt/mp_xfs
-mkdir /mnt/mp_btrfs
-mkdir /mnt/mp_md0
-mkdir /mnt/mp_lvm3
+mkdir /mnt/mp_unaligned_xfs >> /dev/null 2>&1
+mkdir /mnt/mp_ext3 >> /dev/null 2>&1
+mkdir /mnt/mp_ext4 >> /dev/null 2>&1
+mkdir /mnt/mp_xfs >> /dev/null 2>&1
+mkdir /mnt/mp_btrfs >> /dev/null 2>&1
+mkdir /mnt/mp_md0 >> /dev/null 2>&1
+mkdir /mnt/mp_lvm3 >> /dev/null 2>&1
+mkdir /mnt/mp_unaligned_ext3 >> /dev/null 2>&1
+mkdir /mnt/mp_unaligned_ext4 >> /dev/null 2>&1
+mkdir /mnt/mp_unaligned_btrfs >> /dev/null 2>&1
 
-mount /dev/sdb1 /mnt/mp_ext2
-mount /dev/sdb2 /mnt/mp_ext3
-mount /dev/sdb3 /mnt/mp_ext4
-mount /dev/sdb4 /mnt/mp_xfs
-mount /dev/sdc1 /mnt/mp_btrfs
-mount /dev/md0 /mnt/mp_md0
-mount /dev/lvm3/lvm3_ext4 /mnt/mp_lvm3
+mount ${disk1}1 /mnt/mp_unaligned_xfs >> /dev/null 2>&1
+mount ${disk1}2 /mnt/mp_ext3 >> /dev/null 2>&1
+mount ${disk1}3 /mnt/mp_ext4 >> /dev/null 2>&1
+mount ${disk1}4 /mnt/mp_xfs >> /dev/null 2>&1
+mount ${disk2}1 /mnt/mp_btrfs >> /dev/null 2>&1
+mount /dev/md0 /mnt/mp_md0 >> /dev/null 2>&1
+mount /dev/lvm3/lvm3_ext4 /mnt/mp_lvm3 >> /dev/null 2>&1
+mount ${disk3}1 /mnt/mp_unaligned_ext3 >> /dev/null 2>&1
+mount ${disk3}2 /mnt/mp_unaligned_ext4 >> /dev/null 2>&1
+mount ${disk3}3 /mnt/mp_unaligned_btrfs >> /dev/null 2>&1
 
 mdadm --detail --scan >> /etc/mdadm/mdadm.conf >> /dev/null 2>&1
 mdadm --detail --scan >> /etc/mdadm.conf >> /dev/null 2>&1
@@ -168,3 +200,4 @@ mdadm --detail --scan >> /etc/mdadm.conf >> /dev/null 2>&1
 
 cat /proc/mounts | grep 'mp_\|md0' | awk '{print $1,$2,$3}' | awk '{print $0" defaults 0 0"}' >> /etc/fstab
 figlet "completed"
+exit 0
