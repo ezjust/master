6c5ee26d474e7916e22f99f452f145531966a8c5
ezjust <3spiri3@ukr.net>
2018-09-10 17:13:52 +0300

    make many changes

--- own_web.py
+++ own_web.py
@@ -1,10 +1,13 @@
 import SocketServer
 from BaseHTTPServer import BaseHTTPRequestHandler
+from os import popen
 import os
-
-last_com = os.system("git log --name-status HEAD^..HEAD")
+from subprocess import PIPE
+import subprocess
+import shlex
 
 class Handler(BaseHTTPRequestHandler):
+   
     ''' Main class to present webpages and authentication. '''
     def do_HEAD(self):
         print "send header"
@@ -20,6 +23,20 @@ class Handler(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
+        '''Get info about last commit, cpu and memory usage'''
+        git_com = 'git log --name-status HEAD^..HEAD'
+        kwargs = {}
+        kwargs['stdout'] = subprocess.PIPE
+        proc = subprocess.Popen(shlex.split(git_com), **kwargs)
+        stdout_str = proc.communicate()
+
+        pid = os.popen("ps aux | grep 'python own_web.py'| grep -v 'grep' | awk '{print $2}'").readlines()
+        pid = [x.strip() for x in pid]
+        pid = str(pid)
+        pid = pid.translate(None, '[]')
+        resources = os.popen('ps -p' + pid + ' -o %cpu,%mem').readlines()
+        resources = [x.strip() for x in resources]
+
         ''' Present frontpage with user authentication. '''
         if self.headers.getheader('Authorization') == None:
             self.do_AUTHHEAD()
@@ -28,8 +45,8 @@ class Handler(BaseHTTPRequestHandler):
         elif self.headers.getheader('Authorization') == 'Basic dGVzdDp0ZXN0':
             self.do_HEAD()
             self.wfile.write('Authenticated!')
-            self.wfile.write(last_com)
-
+            self.wfile.write("<p>Information about last commit: {0} <p>".format(stdout_str))
+            self.wfile.write("<p>Information about using resources: {0} <p>".format(resources))
             pass
         else:
             self.do_AUTHHEAD()
@@ -37,7 +54,7 @@ class Handler(BaseHTTPRequestHandler):
             self.wfile.write('not authenticated')
             pass
 
-httpd = SocketServer.TCPServer(("", 8080), Handler)
+httpd = SocketServer.TCPServer(("", 8083), Handler)
 
 httpd.serve_forever()
 
